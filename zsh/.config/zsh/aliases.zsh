a8388d38a9ca6189bfcdb51fb711e21037e5dd15
Estefanía <estefaniaramirezmnt@gmail.com>
2023-04-03 19:49:10 +0200

    renabled exa

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -4,9 +4,9 @@
 alias dofi="cd ~/dotfiles && nvim"
 
 # ALIAS COMMANDS
-#alias ls="exa --icons --group-directories-first"
-#alias ll="exa --icons --group-directories-first -l"
-#alias la="exa --icons --group-directories-first -la"
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias la="exa --icons --group-directories-first -la"
 
 alias grep='grep --color'
 
